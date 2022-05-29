3caef139d28f7951a3bcc7d381bb145b1f937252
Oscar Ekesiobi <oscarnipps@gmail.com>
2022-05-29 09:39:16 +0100

    splash screen implementation

--- android/app/src/main/java/com/backbase/assignment/ui/MainActivity.kt
+++ android/app/src/main/java/com/backbase/assignment/ui/MainActivity.kt
@@ -10,6 +10,9 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        setTheme(R.style.AppTheme)
+
         setContentView(R.layout.activity_main)
 
     }
